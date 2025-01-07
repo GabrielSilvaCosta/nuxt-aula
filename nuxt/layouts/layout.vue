34f04760e2aedfeabdc16db54ff092c49610fb24
GabrielSilvaCosta <bob@>
2025-01-07 15:22:48 -0300

    como utilizar img e alis no nuxt

--- nuxt/layouts/layout.vue
+++ nuxt/layouts/layout.vue
@@ -1,5 +1,8 @@
 <template>
   <Header />
+
+  <img src="~/assets/img/image1.jpeg" width="300" height="230" />
+
   <!-- todo conteudo esta dentro do slot -->
   <slot />
   <Footer />
--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -1,4 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// import { resolve } from "path";
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
@@ -8,4 +9,17 @@ export default defineNuxtConfig({
       "Kode Mono": true,
     },
   },
+
+  // este componente faz com que o nuxt reconheça os componentes criados na pasta components
+  // e pode ter pasta na dentro da pasta de componentes que funciona do mesmo jeito sem precisar importar
+  components: [
+    {
+      path: "~/components",
+      pathPrefix: false,
+    },
+  ],
+  // alias: {
+  //   imgs: resolve(__dirname, "./assets/img"),
+  //   "!!": resolve(__dirname, "./node_modules"),
+  // },
 });
